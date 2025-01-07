fa9d492470f7c62956ab67d5a2e70d2979b4c2b4
JaiJai <wm.rd01@gmail.com>
2025-01-07 15:13:38 +0800

    更新0.0.2

--- application.dfcba.js
+++ application.dfcba.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.f58d6.json';
+          this.settingsPath = 'src/settings.8d2e1.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
